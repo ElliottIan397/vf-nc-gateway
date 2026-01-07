12c498066bc1d90a704b516fc67153af1b6f673b
ElliottIan397 <IanElliott@EandSsolutions.com>
2026-01-07 15:15:35 -0600

    tweak update payload
    set RMA to "Received"

--- app.py
+++ app.py
@@ -754,7 +754,7 @@ async def vf_create_rma(body: CreateRmaBody):
         "customer_comments": body.comments or "",
         "uploaded_file_id": 0,
         "staff_notes": "",
-        "return_request_status_id": 0,
+        "return_request_status_id": 10,
         "created_on_utc": created_utc,
         "updated_on_utc": updated_utc        
     }
